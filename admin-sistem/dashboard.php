a72b2425ef7ba4d93db02dad4b037472b0e03f04
hyeonshin <nashrullah1807@gmail.com>
2024-02-08 17:58:21 +0700

    dashboard petugas-pln FE finish

--- admin-sistem/dashboard.php
+++ admin-sistem/dashboard.php
@@ -32,7 +32,7 @@ $grafik_pembayaran = $jumlah_petugas_pembayaran / $total_petugas * 100;
 <body class="sb-nav-fixed">
     <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
         <!-- Navbar Brand-->
-        <a class="navbar-brand ps-3" href="index.html">Tune-der</a>
+        <a class="navbar-brand ps-3" href="#">Tune-der</a>
         <!-- Sidebar Toggle-->
         <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i
                 class="fas fa-bars"></i>
--- admin-sistem/pln/pelanggan.php
+++ admin-sistem/pln/pelanggan.php
@@ -27,7 +27,7 @@ if (!$auth->logged_admin()) {
 <body class="sb-nav-fixed">
 	<nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
 		<!-- Navbar Brand-->
-		<a class="navbar-brand ps-3" href="index.html">Tune-der</a>
+		<a class="navbar-brand ps-3" href="#">Tune-der</a>
 		<!-- Sidebar Toggle-->
 		<button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i
 				class="fas fa-bars"></i>
